ed42a09827d419f6b6667ac6cbdf429bcf59c643
Gregory Pike <greg@gregpike.ca>
2012-09-20 11:11:15 -0400

    Added filename and option for changing the word Upload

--- bootstrap.file-input.js
+++ bootstrap.file-input.js
@@ -11,16 +11,23 @@
 
 */
 $(function() {
-    
+
 $('input[type=file]').each(function(i,elem){
-  
+
+  // Set the word to be displayed on the button
+  var buttonWord = 'Upload';
+
+  if (typeof $(this).attr('title') != 'undefined') {
+    buttonWord = $(this).attr('title');
+  }
+
   // Start by getting the HTML of the input element.
   // Thanks for the tip http://stackoverflow.com/a/1299069
   var input = $('<div>').append( $(elem).eq(0).clone() ).html();
 
   // Now we're going to replace that input field with a Bootstrap button.
   // The input will actually still be there, it will just be float above and transparent (done with the CSS).
-  $(elem).replaceWith('<a class="file-input-wrapper btn">Upload'+input+'</a>');                
+  $(elem).replaceWith('<a class="file-input-wrapper btn">'+buttonWord+input+'</a>');
 })
 // After we have found all of the file inputs let's apply a listener for tracking the mouse movement.
 // This is important because the in order to give the illusion that this is a button in FF we actually need to move the button from the file input under the cursor. Ugh.
@@ -28,11 +35,11 @@ $('input[type=file]').each(function(i,elem){
 
   // As the cursor moves over our new Bootstrap button we need to adjust the position of the invisible file input Browse button to be under the cursor.
   // This gives us the pointer cursor that FF denies us
-  $(".file-input-wrapper").mousemove(function(cursor) {
+  $('.file-input-wrapper').mousemove(function(cursor) {
 
-    var input, wrapper, 
-      wrapperX, wrapperY, 
-      inputWidth, inputHeight, 
+    var input, wrapper,
+      wrapperX, wrapperY,
+      inputWidth, inputHeight,
       cursorX, cursorY;
 
     // This wrapper element (the button surround this file input)
@@ -62,7 +69,15 @@ $('input[type=file]').each(function(i,elem){
       left:moveInputX,
       top:moveInputY
     });
-  });    
+  });
+
+  $('.file-input-wrapper input[type=file]').change(function(){
+
+    // Remove any previous file names
+    $(this).parent().next().has('file-input-name').remove();
+    $(this).parent().after('<span class="file-input-name">'+$(this).val()+'</span>');
+
+  });
 });
-        
+
 });
