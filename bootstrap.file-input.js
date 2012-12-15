62aa3e08a7e844c202451fd2ad28608ec97cf4b2
Rich Lietzke <rich.lietzke@gmail.com>
2012-12-14 21:02:35 -0500

    oooops forgot semi-colon
    Sorry, I think I got this now.

--- bootstrap.file-input.js
+++ bootstrap.file-input.js
@@ -79,7 +79,7 @@ $('input[type=file]').each(function(i,elem){
   $('.file-input-wrapper input[type=file]').change(function(){
 
     // Remove any previous file names
-    $(this).parent().next('.file-input-name').remove()
+    $(this).parent().next().has('file-input-name').remove();
     $(this).parent().after('<span class="file-input-name">'+$(this).val()+'</span>');
 
   });
